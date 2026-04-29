6218865c9ce2359857ad74771a311dd3b7ba99f4
leeoocca <leeoocca@gmail.com>
2026-04-29 12:18:02 +0200

    feat(auth): redirect logged in users navigating to auth pages to /rooms

--- proxy.ts
+++ proxy.ts
@@ -6,16 +6,22 @@ export async function proxy(request: NextRequest) {
 	const session = await auth.api.getSession({
 		headers: await headers(),
 	});
+	const { pathname } = request.nextUrl;
+	const isAuthPage = pathname === "/login" || pathname === "/register";
 
-	if (!session) {
-		return NextResponse.redirect(new URL("/login", request.url));
+	if ((session && !isAuthPage) || (!session && isAuthPage)) {
+		return NextResponse.next();
 	}
 
-	return NextResponse.next();
+	if (session) {
+		return NextResponse.redirect(new URL("/rooms", request.url));
+	}
+
+	return NextResponse.redirect(new URL("/login", request.url));
 }
 
 export const config = {
 	matcher: [
-		"/((?!$|api|_next/static|_next/image|favicon.ico|sitemap.xml|robots.txt|login|register|.*\\.(?:png|svg|jpg)$).*)",
+		"/((?!$|api|_next/static|_next/image|.*\\.(?:png|svg|jpg|txt|xml|ico)$).*)",
 	],
 };

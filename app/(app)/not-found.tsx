c067b4970c571ac232ce33e85f5ecfc3088fd2d7
Fmartu <iperflavio33@gmail.com>
2026-06-22 12:33:37 +0200

    feat: implement global and app-specific 404 error pages with reusable content component

---
+++ app/(app)/not-found.tsx
@@ -0,0 +1,9 @@
+import { NotFoundContent } from "@/components/feature/not-found-content";
+
+export default function AppNotFound() {
+	return (
+		<div className="py-12">
+			<NotFoundContent />
+		</div>
+	);
+}
